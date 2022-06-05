0fc99ed889eb309051f5faeb00f75800ba57b293
Harvey <lihong671@gmail.com>
2022-06-05 09:30:17 +0800

    用户信息

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -43,7 +43,7 @@ class AuthorizationsController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60*24
         ]);
     }
 }
